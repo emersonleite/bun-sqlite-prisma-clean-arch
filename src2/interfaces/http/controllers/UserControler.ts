eab7b57fef1e261fcc685dae2511a979f553a027
Emerson Leite <emerson.bl@gmail.com>
2025-04-12 17:30:25 -0300

    chore: create interfaces

---
+++ src2/interfaces/http/controllers/UserControler.ts
@@ -0,0 +1,30 @@
+import { InputCreateUserDto, OutputCreateUserDto } from "../dto/CreateUserDto";
+import { UserUseCases } from "../../../domain/usecases/UserUseCases";
+import { Context } from "elysia";
+
+export class UserController {
+  constructor(private userUseCases: UserUseCases) {}
+
+  async createUser(
+    ctx: Context
+  ): Promise<OutputCreateUserDto | { message: string }> {
+    const { name, email, password } = (await ctx.body) as InputCreateUserDto;
+
+    const user = await this.userUseCases.createUser({
+      name,
+      email,
+      password,
+    });
+
+    if (!user) {
+      ctx.set.status = 400;
+      return {
+        message: "User already exists.",
+      };
+    }
+
+    ctx.set.status = 201;
+
+    return { ...user };
+  }
+}
