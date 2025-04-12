e3574409b7933fb97a6f3014787352677991aef3
Emerson Leite <emerson.bl@gmail.com>
2025-04-12 17:32:44 -0300

    chore: create auth middleware

---
+++ src2/interfaces/http/middleware/AuthMiddleware.ts
@@ -0,0 +1,25 @@
+import { Context } from "elysia";
+import { AuthService } from "../../../infrastructure/services/AuthService";
+
+export async function authMiddleware(ctx: Context, authService: AuthService) {
+  const authHeader = ctx.request.headers.get("authorization");
+
+  if (!authHeader) {
+    ctx.set.status = 401;
+    return { message: "Authorization header missing" };
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    ctx.set.status = 401;
+    return { message: "Token missing" };
+  }
+
+  try {
+    await authService.verifyToken(token);
+  } catch (err) {
+    ctx.set.status = 401;
+    console.error(err);
+    return { message: "Invalid token" };
+  }
+}
